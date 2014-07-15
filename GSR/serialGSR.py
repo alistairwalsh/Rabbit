486f5c7779d631eeb0dd3517d3006828b84c6aa1
alistairwalsh <alistair.walsh@me.com>
2014-07-15 10:45:49 +1000

    catchall Tuesday 15th July 2014

--- GSR/serialGSR.py
+++ GSR/serialGSR.py
@@ -3,4 +3,9 @@ ser = serial.Serial('/dev/tty.usbmodem000001', 115200, timeout=1)
 s = ser.read(10)        # read up to ten bytes (timeout)
 line = ser.readline()   # read a '\n' terminated line
 print ser.name, s, line,
+
+while True:
+print ser.readline()
+
+
 ser.close()
